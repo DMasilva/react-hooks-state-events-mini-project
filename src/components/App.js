8f765de2d8b6b56bc7790f599de42c99c68ef10e
David <david.owuor@student.moringaschool.com>
2023-01-14 02:23:23 +0300

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,39 @@
-import React from "react";
+import React, {useState} from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
 
 import { CATEGORIES, TASKS } from "../data";
-console.log("Here's the data you're working with");
-console.log({ CATEGORIES, TASKS });
 
 function App() {
+  const[category, setCategory]= useState('All')
+
+  const[tasks, setTasks]=useState(TASKS)
+
+  function addTask(newTask){
+    
+      setTasks([newTask,...tasks])
+    
+  }
+  const filteredNames = tasks.filter((task)=>{
+    if(category === 'All'){
+      return true
+    }else{
+      return category === task.category
+    }
+  })
+
   return (
     <div className="App">
       <h2>My tasks</h2>
-      <CategoryFilter />
-      <NewTaskForm />
-      <TaskList />
+      <CategoryFilter updateCategory={setCategory} categories={CATEGORIES}/>
+      <NewTaskForm onTaskFormSubmit={addTask} categories={CATEGORIES} />
+      <TaskList tasks ={filteredNames}/>
     </div>
   );
 }
 
 export default App;
+
+
+
--- src/components/NewTaskForm.js
+++ src/components/NewTaskForm.js
@@ -1,8 +1,19 @@
 import React from "react";
 
-function NewTaskForm() {
+function NewTaskForm({categories,onTaskFormSubmit}) {
+
+  function submitForm(e){
+    e.preventDefault();
+    const newTask = {
+      text:e.target.text.value,
+      category:e.target.category.value      
+    }
+    onTaskFormSubmit(newTask)
+  }
+
+
   return (
-    <form className="new-task-form">
+    <form onSubmit={submitForm} className="new-task-form">
       <label>
         Details
         <input type="text" name="text" />
@@ -10,7 +21,13 @@ function NewTaskForm() {
       <label>
         Category
         <select name="category">
-          {/* render <option> elements for each category here */}
+          {categories.map((category,index)=>{
+            if(category==='All'){
+              return null
+            }else{
+              return <option key={index}>{category}</option>
+            }
+          })}
         </select>
       </label>
       <input type="submit" value="Add task" />
